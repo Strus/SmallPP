c39017f815b1a61cefc12fd2c84ad074921b2cca
Adrian Kepka <kepka.adrian@gmail.com>
2023-03-05 21:08:17 +0100

    Code cleanup

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "smallpp/object.h"
@@ -15,7 +14,6 @@ struct my_object_vtable {
 };
 
 int my_object_calculate(struct my_object *self) { return self->some_value * 2; }
-void my_object_temp(struct spp_object *self) {}
 
 struct my_object *my_object_create(int val) {
   struct my_object *self = malloc(sizeof(struct my_object));
@@ -39,33 +37,3 @@ int main() {
   spp_vcall(spp_object, obj, destroy);
   return 0;
 }
-
-#if 0
-struct my_object_derived {
-  struct my_object super;
-  int some_other_value;
-};
-
-void my_object_derived_describe(struct my_object *self) {
-  printf("This is my object some_value, but different: %d\n",
-         self->some_value * 3);
-}
-
-struct my_object_derived *my_object_derived_create(int val, int other_val) {
-  struct my_object_derived *self = malloc(sizeof(struct my_object_derived));
-  self->super = *my_object_create(val);
-
-  static const struct my_object_vtable vtable = {
-      .super =
-          {
-              .destroy = spp_object_destroy,
-          },
-      .describe = my_object_derived_describe,
-  };
-  self->super.super.vtable = (void *)&vtable;
-
-  self->some_other_value = other_val;
-
-  return self;
-}
-#endif
