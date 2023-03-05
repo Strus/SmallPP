e4eed6d7df4edb848e66e6cb870579597322bdf6
Adrian Kepka <kepka.adrian@gmail.com>
2023-03-05 21:46:41 +0100

    Introduce clang-format

--- include/smallpp/object.h
+++ include/smallpp/object.h
@@ -1,39 +1,41 @@
 #include <stdlib.h>
 
 struct spp_object {
-  void *vtable;
+    void *vtable;
 };
 
 struct spp_object_vtable {
-  void (*destroy)(struct spp_object *);
-  int (*hash)(struct spp_object *);
+    void (*destroy)(struct spp_object *);
+    int (*hash)(struct spp_object *);
 };
 
-void spp_object_destroy(struct spp_object *self) { free(self); }
-int spp_object_hash(struct spp_object *self) { return 0; }
-
-struct spp_object *spp_object_create() {
-  struct spp_object *self = malloc(sizeof(struct spp_object));
-
-  static const struct spp_object_vtable vtable = {
-      .destroy = spp_object_destroy,
-      .hash = spp_object_hash,
-  };
-  self->vtable = (void *)&vtable;
-
-  return self;
+void spp_object_destroy(struct spp_object *self) {
+    free(self);
+}
+int spp_object_hash(struct spp_object *self) {
+    return 0;
 }
 
-#define spp_init_vtable(self, vtable, super_type)                              \
-  size_t methods_count = sizeof(struct super_type##_vtable) / sizeof(void *);  \
-  for (size_t i = 0; i < methods_count; i++) {                                 \
-    if (!((void **)(&(vtable)))[i]) {                                          \
-      ((void **)(&(vtable)))[i] =                                              \
-          ((void **)(((struct spp_object *)self)->vtable))[i];                 \
-    }                                                                          \
-  }                                                                            \
-  ((struct spp_object *)self)->vtable = (void *)&vtable;
+struct spp_object *spp_object_create() {
+    struct spp_object *self = malloc(sizeof(struct spp_object));
 
-#define spp_vcall(type, object, func, ...)                                     \
-  ((struct type##_vtable *)((struct spp_object *)object)->vtable)              \
-      ->func((struct type *)object, ##__VA_ARGS__)
+    static const struct spp_object_vtable vtable = {
+        .destroy = spp_object_destroy,
+        .hash = spp_object_hash,
+    };
+    self->vtable = (void *)&vtable;
+
+    return self;
+}
+
+#define spp_init_vtable(self, vtable, super_type)                                                                      \
+    size_t methods_count = sizeof(struct super_type##_vtable) / sizeof(void *);                                        \
+    for (size_t i = 0; i < methods_count; i++) {                                                                       \
+        if (!((void **)(&(vtable)))[i]) {                                                                              \
+            ((void **)(&(vtable)))[i] = ((void **)(((struct spp_object *)self)->vtable))[i];                           \
+        }                                                                                                              \
+    }                                                                                                                  \
+    ((struct spp_object *)self)->vtable = (void *)&vtable;
+
+#define spp_vcall(type, object, func, ...)                                                                             \
+    ((struct type##_vtable *)((struct spp_object *)object)->vtable)->func((struct type *)object, ##__VA_ARGS__)
--- main.c
+++ main.c
@@ -4,76 +4,78 @@
 #include "smallpp/object.h"
 
 struct my_object {
-  struct spp_object super;
-  int some_value;
+    struct spp_object super;
+    int some_value;
 };
 
 struct my_object_vtable {
-  struct spp_object_vtable super;
-  int (*calculate)(struct my_object *);
+    struct spp_object_vtable super;
+    int (*calculate)(struct my_object *);
 };
 
-int my_object_calculate(struct my_object *self) { return self->some_value * 2; }
+int my_object_calculate(struct my_object *self) {
+    return self->some_value * 2;
+}
 
 struct my_object *my_object_create(int val) {
-  struct my_object *self = malloc(sizeof(struct my_object));
-  self->super = *spp_object_create();
+    struct my_object *self = malloc(sizeof(struct my_object));
+    self->super = *spp_object_create();
 
-  static struct my_object_vtable vtable = {
-      .calculate = my_object_calculate,
-  };
-  spp_init_vtable(self, vtable, spp_object);
+    static struct my_object_vtable vtable = {
+        .calculate = my_object_calculate,
+    };
+    spp_init_vtable(self, vtable, spp_object);
 
-  self->some_value = val;
+    self->some_value = val;
 
-  return self;
+    return self;
 }
 
 struct derived_object {
-  struct my_object super;
-  int different_value;
+    struct my_object super;
+    int different_value;
 };
 
 struct derived_object_vtable {
-  struct my_object_vtable super;
-  int (*get_different_value)(struct derived_object *);
+    struct my_object_vtable super;
+    int (*get_different_value)(struct derived_object *);
 };
 
 int derived_object_calculate(struct my_object *self) {
-  return self->some_value * ((struct derived_object *)self)->different_value;
+    return self->some_value * ((struct derived_object *)self)->different_value;
 }
 
 int derived_object_get_different_value(struct derived_object *self) {
-  return self->different_value;
+    return self->different_value;
 }
 
 struct derived_object *derived_object_create(int val, int val2) {
-  struct derived_object *self = malloc(sizeof(struct derived_object));
-  self->super = *my_object_create(val);
+    struct derived_object *self = malloc(sizeof(struct derived_object));
+    self->super = *my_object_create(val);
 
-  static struct derived_object_vtable vtable = {
-      .super =
-          {
-              .calculate = derived_object_calculate,
-          },
-      .get_different_value = derived_object_get_different_value,
-  };
-  spp_init_vtable(self, vtable, my_object);
+    static struct derived_object_vtable vtable = {
+        .super =
+            {
+                .calculate = derived_object_calculate,
+            },
+        .get_different_value = derived_object_get_different_value,
+    };
+    spp_init_vtable(self, vtable, my_object);
 
-  self->different_value = val2;
+    self->different_value = val2;
 
-  return self;
+    return self;
 }
 
 int main() {
-  struct my_object *obj = my_object_create(5);
-  printf("%d\n", spp_vcall(my_object, obj, calculate));
-  printf("%d\n", spp_vcall(spp_object, obj, hash));
+    struct my_object *obj = my_object_create(5);
+    printf("%d\n", spp_vcall(my_object, obj, calculate));
+    printf("%d\n", spp_vcall(spp_object, obj, hash));
 
-  struct derived_object *der = derived_object_create(5, 3);
-  printf("%d\n", spp_vcall(my_object, der, calculate));
-  printf("%d\n", spp_vcall(derived_object, der, get_different_value));
+    struct derived_object *der = derived_object_create(5, 3);
+    printf("%d\n", spp_vcall(my_object, der, calculate));
+    printf("%d\n", spp_vcall(derived_object, der, get_different_value));
 
-  spp_vcall(spp_object, obj, destroy);
-  return 0;
+    spp_vcall(spp_object, obj, destroy);
+    return 0;
 }
