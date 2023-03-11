fa76f835cd94b6c95c82c99d8a8378441a2e6d24
Adrian Kepka <kepka.adrian@gmail.com>
2023-03-11 23:01:01 +0100

    Add dynamic allocation in single_inheritance example
    Dynamic allocation allows us to show how to override implementation
    of a base class method, as we need to override spp_object::destroy()

--- examples/single_inheritance/my_object.c
+++ examples/single_inheritance/my_object.c
@@ -3,7 +3,8 @@
 
 struct my_object {
     struct spp_object super;
-    int some_value;
+    int *array;
+    int len;
 };
 
 struct my_object_vtable {
@@ -11,19 +12,29 @@ struct my_object_vtable {
     int (*calculate)(struct my_object *);
 };
 
+void my_object_destroy(struct spp_object *super) {
+    struct my_object *self = (struct my_object *)super;
+    free(self->array);
+}
+
 int my_object_calculate(struct my_object *self) {
-    return self->some_value * 2;
+    return self->len * 2;
 }
 
 void my_object_init(struct my_object *self, int val) {
     spp_object_init(&self->super);
 
     static struct my_object_vtable vtable = {
+        .super =
+            {
+                .destroy = my_object_destroy,
+            },
         .calculate = my_object_calculate,
     };
     spp_init_vtable(self, vtable, spp_object);
 
-    self->some_value = val;
+    self->array = malloc(sizeof(int) * val);
+    self->len = val;
 }
 
 struct my_object *my_object_create(int val) {
