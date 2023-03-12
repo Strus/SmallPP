e915c3eef406784d23f9ec3cc47eba4c9db8395b
Adrian Kepka <kepka.adrian@gmail.com>
2023-03-12 13:10:41 +0100

    Add docstrings

--- include/smallpp/object.h
+++ include/smallpp/object.h
@@ -1,19 +1,50 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+/**
+ * Base object.
+ *
+ * Every custom object should inherit from it.
+ */
 struct spp_object {
     void *vtable;
 };
 
+/**
+ * Base object vtable.
+ *
+ * Contains all object-common methods.
+ */
 struct spp_object_vtable {
+    /**
+     * Destructor.
+     *
+     * All memory should be freed inside it.
+     * You need to manually call the super destructor in custom destructor.
+     */
     void (*destroy)(struct spp_object *);
+
+    /**
+     * Calculate hash.
+     */
     unsigned long (*hash)(struct spp_object *);
 };
 
+/**
+ * Destructor.
+ */
 void spp_object_destroy(struct spp_object *self);
 
+/**
+ * Calculate hash.
+ */
 unsigned long spp_object_hash(struct spp_object *self);
 
+/**
+ * Initializer.
+ *
+ * Should be called in custom object constructor.
+ */
 void spp_object_init(struct spp_object *self);
 
 #endif // OBJECT_H
--- include/smallpp/smallpp.h
+++ include/smallpp/smallpp.h
@@ -3,6 +3,17 @@
 
 #include <stdlib.h>
 
+/**
+ * Initialize object vtable.
+ *
+ * Allocate memory for the vtable structure and copy all not implemented
+ * super class function pointers.
+ *
+ * @param self
+ * @param vtable     vtable object, should already contain all function pointers
+ *                   and have functions that should be inherited pointing to NULL
+ * @param super_type name of the super type
+ */
 #define spp_init_vtable(self, vtable, super_type)                                                                      \
     size_t methods_count = sizeof(struct super_type##_vtable) / sizeof(void *);                                        \
     for (size_t i = 0; i < methods_count; i++) {                                                                       \
@@ -12,9 +23,17 @@
     }                                                                                                                  \
     ((struct spp_object *)self)->vtable = (void *)&vtable;
 
+/**
+ * Call virtual method.
+ */
 #define spp_vcall(type, object, func, ...)                                                                             \
     ((struct type##_vtable *)((struct spp_object *)object)->vtable)->func((struct type *)object, ##__VA_ARGS__)
 
+/**
+ * Helper macro for object create boilerplate.
+ *
+ * Allocates memory for the given struct and call object init.
+ */
 #define spp_create(type, ...)                                                                                          \
     malloc(sizeof(struct type));                                                                                       \
     type##_init(self, ##__VA_ARGS__);
